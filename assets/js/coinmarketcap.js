611babaeaa00225bc1f088fced919664873613e4
johnston9 <davejohnston9@gmail.com>
2020-11-03 00:45:22 +0000

    Started API fetch

--- assets/js/coinmarketcap.js
+++ assets/js/coinmarketcap.js
@@ -1,29 +1,25 @@
-let url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/info",
-//listings/latest"
-//quotes/latest"
+//let dataDiv = document.getElementById("data").textContent
+fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD")
+ .then(response => response.json())
+ .then(function(resp) {console.log(resp)})
+ //.then(function(resp) {dataDiv = resp})
 
-qString= "?CMC_PRO_API_KEY=38158a11-2401-4362-8de7-e3be6952b8b4"
-//+ "&symbol=BTC"
-//+ "&start=1&limit=5&convert=USD"
-//+ "&symbol=BTC"
-//+ "&symbol=BTC&convert=BTC"
-fetch(url + qString)
-.then(response => response.json())
- .then(resp => console.log(resp.data)) 
-  .catch(err => console.log('Request Failed', err))  
-
-$.getJSON("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+  //.then(resp => $('#data').html(resp) )
+ //.then(resp => dataDiv.textContent(resp))
+  //.catch(err => console.log('Request Failed', err))  
+/*$.when(
+$.getJSON("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"))
+    .then(response => console.log(response)) 
     .then(function(response) {
-            $("#gh-user-data").html(response);   },
-
-
-
- /*...or
-let response = await fetch(url);
+            $("#data").html(response);})*/
+ 
+/*let response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD");
 if (response.ok) { 
   let result = await response.json();
 } else {
-  alert("HTTP-Error: " + response.status);}...*/
+  alert("HTTP-Error: " + response.status);}
+  console.log(result)*/
+
 
 
 
