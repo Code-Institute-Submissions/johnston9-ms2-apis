9b79b21d066f9f9cef0e8e15ee0e724e9eebb4e2
johnston9 <davejohnston9@gmail.com>
2020-11-04 00:49:08 +0000

    Added easy version of memgame in guess.js file and guess.html. Began fetch function in js file

--- assets/js/coinmarketcap.js
+++ assets/js/coinmarketcap.js
@@ -1,10 +1,34 @@
-//let dataDiv = document.getElementById("data").textContent
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD")
- .then(response => response.json())
- .then(function(resp) {console.log(resp)})
- //.then(function(resp) {dataDiv = resp})
 
-  //.then(resp => $('#data').html(resp) )
+function fetchdata(coin) {
+let dataDiv = document.getElementById("data") 
+let url = "https://api.coingecko.com/api/v3"
+let qstring = "/simple/price?ids=" + coin + "&vs_currencies=USD"
+fetch(url + qstring)
+ .then(response => response.json())
+ .then(function(resp) {dataDiv.innerHTML = resp.bitcoin.usd
+  return resp})
+ .then(function(resp) {console.log(resp.bitcoin.usd)})}
+ 
+ fetchdata("bitcoin")
+
+ /*function coinbase() {
+   let dataDiv = document.getElementById("data")
+    fetch('https://coinbase.com/api/v1/prices/buy')
+    .then(response => response.json())
+ .then(function(resp) {dataDiv.innerHTML = resp
+  return resp})
+ .then(function(resp) {console.log(resp)})
+ }
+     coinbase()*/
+    
+
+
+
+
+  
+  
+//fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD")
+ //.then(resp => $('#data').html(resp) )
  //.then(resp => dataDiv.textContent(resp))
   //.catch(err => console.log('Request Failed', err))  
 /*$.when(
---
+++ assets/memgame/apptwo.js
@@ -0,0 +1,169 @@
+/*preshow*/
+const frontImages = [
+  "assets/memgame/images/cw.png",
+  "assets/memgame/images/unicorn1.png",
+  "assets/memgame/images/cw2.png",
+  "assets/memgame/images/unicorn1.png",
+  "assets/memgame/images/hqi.png",
+  "assets/memgame/images/unicorn1.png",
+  "assets/memgame/images/hq2.png",
+  "assets/memgame/images/unicorn1.png",
+  "assets/memgame/images/sf1",
+  "assets/memgame/images/unicorn1.png",
+  "assets/memgame/images/wr1",
+  "assets/memgame/images/unicorn1.png",
+];
+let faces = document.querySelectorAll(".card-front-face img");
+
+function flashImage() {
+  return frontImages[Math.floor(Math.random() * 12)];
+}
+
+function flashCard() {
+  let randomface = faces[Math.floor(Math.random() * 12)];
+  randomface.setAttribute("src", flashImage());
+}
+
+function moveCard() {
+  timerId = setInterval(flashCard, 500);
+}
+moveCard();
+
+/*clock*/
+let timerId2 
+let clocktime = document.getElementById("clock");
+let currentTime = clocktime.textContent; 
+let timebut = document.getElementById("time");
+timebut.addEventListener("click", startClock);
+//let resetbut = document.getElementById("resetClock");
+//resetbut.addEventListener("click", resetTime);
+
+function startClock() {
+  clocktime.style.color = "black";
+  timerId2 = setInterval(countDown, 1000);
+}
+
+function countDown() {
+  currentTime--;
+  clocktime.textContent = currentTime;
+
+  if (currentTime === 0) { 
+    alert("GAME OVER!");
+    currentTime = 60;
+    clearInterval(timerId2);
+  } else if (winners.length === 2) {
+       alert("Winner Claim your prize");
+    currentTime = 60;
+    clearInterval(timerId2);
+  }
+}
+function startClock() {
+  clocktime.style.color = "black";
+  timerId2 = setInterval(countDown, 1000);
+}
+
+/*function resetTime() {
+currentTime = 61;
+clearInterval(timerId2);
+}*/
+
+/*play*/
+winners = []
+var play = document.querySelector("#play");
+play.addEventListener("click", shuffle);
+var boxes = document.querySelectorAll(".card-box");
+
+function shuffle() {
+  //clearTimeout(timerId);
+  clearInterval(timerId); 
+  clocktime.style.color = "red";
+  currentTime = 60;
+  clearInterval(timerId2);
+  
+  faces.forEach((face) => {
+    face.setAttribute("src", "assets/memgame/images/unicorn1.png");
+  });
+
+  /*for(let i = 0; i < winners.length; i++) {
+       win.addEventListener("click", flip)}*/
+
+  winners.forEach((win) => {
+      win.addEventListener("click", flip)
+  })
+  winners.forEach((win) => {
+      win.classList.remove("is-flipped");
+ }) 
+
+ winners = []
+  
+  boxes.forEach((box) => {
+    let ramPos = Math.floor(Math.random() * 12);
+    box.style.order = ramPos;
+  });
+  
+  reset(); 
+}
+
+/*Flip*/
+
+var cards = document.querySelectorAll(".card");
+let flippedCard = false;
+let freezeGame = false;
+let cardOne, cardTwo;
+
+function flip() {
+  if (freezeGame) return;
+  if (this === cardOne) return;
+  this.classList.add("is-flipped");
+  if (!flippedCard) {
+    flippedCard = true;
+    cardOne = this;
+  } else {
+    flippedCard = false;
+    cardTwo = this;
+
+    checkForMatch();
+  }
+}
+
+function checkForMatch() {
+  if (cardOne.dataset.name === cardTwo.dataset.name) {
+    freezeCards();
+  } else {
+    unflipCards();
+  }
+}
+
+function freezeCards() {
+  cardOne.removeEventListener("click", flip);
+  winners.push(cardOne, cardTwo)
+  cardTwo.removeEventListener("click", flip);
+  if (winners.length === 12) {
+         alert("Winner. Claim your prize")
+    }
+  reset();
+} 
+
+/*function win() {
+     if (winners.length === 12) {
+         alert("Winner. Claim your prize")
+    }
+}*/
+
+function unflipCards() {
+  freezeGame = true;
+  setTimeout(() => {
+    cardOne.classList.remove("is-flipped");
+    cardTwo.classList.remove("is-flipped");
+    reset();
+  }, 1500);
+}
+
+function reset() {
+  flippedCard = false;
+  freezeGame = false;
+  cardOne = null;
+  cardTwo = null;
+}
+
+cards.forEach((card) => card.addEventListener("click", flip));
