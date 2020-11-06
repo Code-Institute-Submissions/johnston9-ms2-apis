466098ad35fd2414a75f0adb3ac97af8de2d910c
johnston9 <davejohnston9@gmail.com>
2020-11-06 18:16:54 +0000

    Added nrw marker code to map.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
- function initMap() {
+ /*function initMap() {
         var map = new google.maps.Map(document.getElementById("map1"), {
           zoom: 3,
           center: {
@@ -30,4 +30,43 @@
         var markerCluster = new MarkerClusterer(map, markers, {
                 imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
             });
+      }*/
+
+      /*..........map>*/
+      function initMap() {
+          const mapProp = {
+              center: {
+            lat: 46.619261,
+            lng: -33.134766,
+          },
+          /*center: new google.maps.LatLng(41.084045, -73.874256),*/
+          zoom: 5
       }
+      var map = new google.maps.Map(document.getElementById("map1"), mapProp)
+      }
+
+    
+      
+
+      /*var myMarks = [{
+                lat: 40.785091,
+                lng: -73.968285,
+                name: "yo"
+            }, {
+                lat: 41.084045,
+                lng: -73.874256,
+                name: "yoyo"
+            }, {
+                lat: 40.754932,
+                lng: -73.984016,
+                name: "yol"
+            }]
+
+    for( let i = 0; i < myMarks.length; i++) {
+        const marker = new google.maps.Marker(
+        {position: new google.maps.latLng(myMarks[i].lat, myMarks[i].lng),
+        map: map,
+        title: myMarks[i].name})
+        marker.setMap(map)
+    }*/
+
