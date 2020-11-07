420412dd0ade81f4fbde671b189be1c70224d7cb
johnston9 <davejohnston9@gmail.com>
2020-11-07 01:19:03 +0000

    Added new wireframes, added new map.js code.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,14 @@
- /*function initMap() {
+ 
+ function initMap() {
         var map = new google.maps.Map(document.getElementById("map1"), {
           zoom: 3,
           center: {
-            lat: 46.619261,
-            lng: -33.134766,
+            lat: 41.084045, 
+            lng: -73.874256,
           },
-        });
+            });
+        }
+        
 
         var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
@@ -23,43 +26,37 @@
         var markers = locations.map(function(location, i) {
                 return new google.maps.Marker({
                     position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+                  label: labels[i % labels.length]   });   });
+    
 
         var markerCluster = new MarkerClusterer(map, markers, {
                 imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
             });
-      }*/
 
-      /*..........map>*/
-      function initMap() {
+          /*..........map>*/
+      /*function initMap() {
           const mapProp = {
-              center: {
-            lat: 46.619261,
-            lng: -33.134766,
-          },
-          /*center: new google.maps.LatLng(41.084045, -73.874256),*/
-          zoom: 5
+             center: new google.maps.LatLng(41.084045, -73.874256),
+             zoom: 5
       }
       var map = new google.maps.Map(document.getElementById("map1"), mapProp)
       }
 
-    
-      
-
-      /*var myMarks = [{
-                lat: 40.785091,
-                lng: -73.968285,
-                name: "yo"
+    var myMarks = [{
+                "lat": 40.785091,
+                "lng": -73.968285,
+                "name": "one",
+                "info": "Best in town"
             }, {
-                lat: 41.084045,
-                lng: -73.874256,
-                name: "yoyo"
+                "lat": 41.084045,
+                "lng": -73.874256,
+                "name": "two",
+                "info": "Best in town"
             }, {
-                lat: 40.754932,
-                lng: -73.984016,
-                name: "yol"
+                "lat": 40.754932,
+                "lng": -73.984016,
+                "name": "three",
+                "info": "Best in town"
             }]
 
     for( let i = 0; i < myMarks.length; i++) {
@@ -68,5 +65,8 @@
         map: map,
         title: myMarks[i].name})
         marker.setMap(map)
-    }*/
+    }*
 
+    
+
+  
