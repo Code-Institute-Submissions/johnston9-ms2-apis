82ecc639e9cc8b3bd61f27c4c52e6f73528587f0
johnston9 <davejohnston9@gmail.com>
2020-11-09 01:34:41 +0000

    Added code to cyber.js to link fetch call from imput on index page, tried both frtch and jQuery get.

--- assets/js/cyber1.js
+++ assets/js/cyber1.js
@@ -1,17 +1,72 @@
 
-function fetchdata(coin) {
-let dataDiv = document.getElementById("data") 
+/*function fetchdata(coin) {
+let dataDiv = document.getElementById("data")
+//let dataCoin = document.getElementById("coin-data")  
 let url = "https://api.coingecko.com/api/v3"
-let qstring = "/simple/price?ids=" + coin + "&vs_currencies=USD"
+let qstring = "/simple/price?ids=" + ${coin} + "&vs_currencies=USD"
 fetch(url + qstring)
  .then(response => response.json())
  .then(function(resp) {dataDiv.innerHTML = resp[coin].usd
   return resp})
  .then(function(resp) {console.log(resp[coin].usd)})
-
 }
  
- fetchdata("bitcoin")
+ //fetchdata("bitcoin")*/
+
+ 
+/*function fetchdata(coin) {
+    //$("#data").html("");
+    var coin = $("#coin").val();
+    //console.log(coin)
+let dataDiv = document.getElementById("data")
+//let dataCoin = document.getElementById("coin-data")  
+let url = `https://api.coingecko.com/api/v3`
+let qstring = `/simple/price?ids=${coin}&vs_currencies=USD`
+fetch(url + qstring)
+ .then(response => response.json())
+ .then(function(resp) {dataDiv.innerHTML = resp
+  return resp})
+ .then(function(resp) {console.log(resp)})
+}*/
+//fetchdata("bitcoin")
+
+/*...........query....*/
+ $(document).ready(function() { 
+     function fetchdata(event) {
+    $("#data").html("");
+    var coin = $("#coin").val();
+
+    /*$("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);*/
+
+    $.when(
+           $.getJSON(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=USD`),
+    ).then(
+        function(response) {
+            var userData = response;
+            $("#data").html(userData);
+        },
+        /*function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            }  else if
+             (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } else {
+                console.log(errorResponse);
+                $("#data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }*/
+        
+)}
+});
+
+//$(document).ready(fetchGitHubInformation);
+
  
  
 
