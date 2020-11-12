3ce432bda01b68c23e10803c9d5a8f269f49a76f
johnston9 <davejohnston9@gmail.com>
2020-11-12 01:01:59 +0000

    Styled navlogo.

---
+++ assets/js/alert.js
@@ -0,0 +1,5 @@
+
+document.getElementById("signin").addEventListener("click", displayAlert)
+function displayAlert() {
+  alert("Must be a registered student to sign in. Please go to Contact page to get started");
+}
