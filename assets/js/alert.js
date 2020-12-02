76f5e5ad934fa331575fb19dadc4106f0ee7e80e
johnston9 <davejohnston9@gmail.com>
2020-12-02 19:24:26 +0000

    Removed nav sections on pages, put return to top button instead for index and courses pages

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -6,23 +6,22 @@ function displayAlert() {
 
 /*-----return to top-----*/
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
+topbutton = document.getElementById("topbut");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+topbutton.addEventListener("click", poptop)
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+window.onscroll = function() {buttonDisplay()};
+
+function buttonDisplay() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topbutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    topbutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function poptop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
 }
 
